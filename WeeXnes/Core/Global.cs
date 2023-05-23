68d59943a72e8ccf08a9a65d2d6a8de0064f630c
WeeXnes <somtrigi@gmail.com>
2023-05-23 18:39:16 +0200

    changed to x64 for coming changes

--- WeeXnes/Core/Global.cs
+++ WeeXnes/Core/Global.cs
@@ -8,7 +8,7 @@ namespace WeeXnes.Core
 {
     public class Information
     {
-        public const string Version = "4.2.6";
+        public const string Version = "4.2.7";
         public const string EncryptionHash = "8zf5#RdyQ]$4x4_";
         public const string ApiUrl = "https://api.github.com/repos/weexnes/weexnessuite/releases/latest";
     }
