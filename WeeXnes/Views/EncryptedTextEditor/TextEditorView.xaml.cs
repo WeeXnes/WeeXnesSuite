5cb3c52ba76a645f05df392b1a6bc781e7a508f7
WeeXnes <somtrigi@gmail.com>
2024-06-13 12:46:11 +0200

    file editor update

--- WeeXnes/Views/EncryptedTextEditor/TextEditorView.xaml.cs
+++ WeeXnes/Views/EncryptedTextEditor/TextEditorView.xaml.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Forms;
+using EncryptionLib;
+using WeeXnes.Core;
 
 namespace WeeXnes.Views.EncryptedTextEditor;
 
 public partial class TextEditorView : Page
 {
+    public string currentFilePath { get; set; } = null;
     public TextEditorView()
     {
         InitializeComponent();
@@ -26,18 +31,42 @@ public partial class TextEditorView : Page
 
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                
+                this.currentFilePath = openFileDialog.FileName;
+                string[] FileContent = File.ReadAllLines(openFileDialog.FileName);
+                string[] decryptedContent = EncryptorLibary.decryptArray(Information.EncryptionHash, FileContent);
+                rtb_FileEditor.Document.Blocks.Clear();
+                foreach (string line in decryptedContent)
+                {
+                    rtb_FileEditor.Document.Blocks.Add(new Paragraph(new Run(line)));
+                }
             }
         }
     }
 
     private void Btn_saveFile_OnClick(object sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        if(this.currentFilePath == null)
+            return;
     }
 
-    private void flkpw(object sender, RoutedEventArgs e)
+    private void Btn_saveFileAs_OnClick(object sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+        {
+            this.currentFilePath = saveFileDialog.FileName;
+            saveFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            saveFileDialog.Filter = "WXN Text Files (*.wtf)|*.wtf";
+            saveFileDialog.RestoreDirectory = true ;
+
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                TextRange textRange = new TextRange(rtb_FileEditor.Document.ContentStart, rtb_FileEditor.Document.ContentEnd);
+                string plainText = textRange.Text;
+                string[] lines = plainText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                string[] encryptedContent =
+                    EncryptionLib.EncryptorLibary.encryptArray(Information.EncryptionHash, lines);
+                File.WriteAllLines(saveFileDialog.FileName, encryptedContent);
+            }
+        }
     }
 }
