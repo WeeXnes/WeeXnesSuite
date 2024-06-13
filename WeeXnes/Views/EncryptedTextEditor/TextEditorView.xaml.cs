f65057350da5a3536601c00e6363892f3a90dfe4
WeeXnes <somtrigi@gmail.com>
2024-06-13 11:36:45 +0200

    File Editor Update

--- WeeXnes/Views/EncryptedTextEditor/TextEditorView.xaml.cs
+++ WeeXnes/Views/EncryptedTextEditor/TextEditorView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace WeeXnes.Views.EncryptedTextEditor;
 
@@ -7,5 +8,20 @@ public partial class TextEditorView : Page
     public TextEditorView()
     {
         InitializeComponent();
+    }
+
+    private void Btn_openFile_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Btn_saveFile_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void flkpw(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
